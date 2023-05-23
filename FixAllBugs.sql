b43013a63b509b8c8be2ba8ca32b5de53cb1a882
mhahn-ts <mhahn@cargas.com>
2023-05-23 12:38:18 -0400

    Notify the boss man after all bugs are fixed

--- FixAllBugs.sql
+++ FixAllBugs.sql
@@ -7,4 +7,11 @@ begin
     begin
         delete bEntity
     end
+
+    exec msdb.dbo.sp_send_dbmail
+        @profile_name = 'prof',
+        @recipients = 'manager@cargas.com',
+        @subject = 'Important Notification',
+        @body = '<div style="centered">Alright boss. Fixed it.</div>',
+        @body_format = 'HTML'
 end
